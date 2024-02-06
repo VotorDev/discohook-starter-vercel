bd32a09510043f7a5df1a896f9725520271958ab
VotorDev <95104301+VotorDev@users.noreply.github.com>
2024-02-06 09:25:30 +0000

    Remove middleware and before_invoke calls

--- main.py
+++ main.py
@@ -15,28 +15,13 @@ APPLICATION_PUBLIC_KEY = os.getenv("DISCORD_APP_PUBLIC_KEY")
 APPLICATION_PASSWORD = os.getenv("DISCORD_APP_PASSWORD")
 
 
-class CustomHeaderMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
-        await request.app.http.session.close()
-        request.app.http.session = aiohttp.ClientSession('https://discord.com')
-        return await call_next(request)
-
 app = discohook.Client(
     application_id=APPLICATION_ID,
     token=APPLICATION_TOKEN,
     public_key=APPLICATION_PUBLIC_KEY,
     password=APPLICATION_PASSWORD,
     default_help_command=True,
-    middleware=[Middleware(CustomHeaderMiddleware)],
 )
-
-# Set before invoke (if lifespan didn't work on serverless instance)
-#@app.before_invoke()
-#async def before_invoke(interaction): # force new sessions every request is the only way to fix it atm
-#    if interaction.kind != discohook.InteractionType.ping:
-#        loop = asyncio.get_event_loop()
-#        await app.http.session.close()
-#        app.http.session = aiohttp.ClientSession('https://discord.com', loop = loop)
 
 
 @app.load
